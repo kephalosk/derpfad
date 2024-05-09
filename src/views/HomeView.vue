0de6105a1af093edeec1b575436d43941a30eeec
kephalosk <watchfromtheshore@gmail.com>
2024-05-09 19:53:29 +0200

    fix merge conflict

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,30 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-<<<<<<< Updated upstream
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-=======
     <HomeComponent msg="Willkommen beim Pfad" />
->>>>>>> Stashed changes
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-<<<<<<< Updated upstream
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
-=======
 import HomeComponent from "@/components/HomeComponent.vue"; // @ is an alias to /src
->>>>>>> Stashed changes
 
 export default defineComponent({
   name: "HomeView",
   components: {
-<<<<<<< Updated upstream
-    HelloWorld,
-=======
     HomeComponent,
->>>>>>> Stashed changes
   },
 });
 </script>
