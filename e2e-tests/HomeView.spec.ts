3ffcbaf91d7b0761137aad868a1c7a000179d7c4
kephalosk <watchfromtheshore@gmail.com>
2024-06-06 14:04:21 +0200

    fix e2e-tests

--- e2e-tests/HomeView.spec.ts
+++ e2e-tests/HomeView.spec.ts
@@ -7,16 +7,17 @@ test('has title', async ({ page }) => {
   await expect(page).toHaveTitle('derpfad');
 });
 
-test('has about link', async ({ page }) => {
+test('has Chapter 1: Basics link', async ({ page }) => {
   await page.goto(config.BASEPATH);
 
-  await page.getByRole('link', { name: 'About' }).click();
+  await page.getByRole('link', { name: 'Chapter 1: Basics' }).click();
 
-  await expect(page.getByRole('heading', { name: 'This is an about page' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'The Easy Path To Master Web Development' })).toBeVisible();
 });
 
 test('has InfoCard', async ({ page }) => {
   await page.goto(config.BASEPATH);
-  const name = await page.innerText('.rootInfoCard');
-  expect(name).toContain('User Interface');
+  const name = await page.innerText('.cardPersonalComputer');
+  console.log(name);
+  expect(name).toContain('Personal Computer');
 });
